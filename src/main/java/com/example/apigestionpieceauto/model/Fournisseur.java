6dcd28ea4fa84fa5c1ad5bfbe9a1f6dde0e04005
jeremy <jeremyhoarau44@gmail.com>
2025-05-15 11:32:03 +0400

    Modification des entités Fournisseur et Vehicule

--- src/main/java/com/example/apigestionpieceauto/model/Fournisseur.java
+++ src/main/java/com/example/apigestionpieceauto/model/Fournisseur.java
@@ -2,13 +2,23 @@ package com.example.apigestionpieceauto.model;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "fournisseur")
 public class Fournisseur {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    private String nom;
+    private String adresse;
+    private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<PieceAuto> pieceAutos;
 
     public Long getId() {
         return id;
--- src/main/java/com/example/apigestionpieceauto/model/Vehicule.java
+++ src/main/java/com/example/apigestionpieceauto/model/Vehicule.java
@@ -2,13 +2,41 @@ package com.example.apigestionpieceauto.model;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+import java.util.Set;
+
 @Entity
 @Table(name = "vehicule")
 public class Vehicule {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    private String marque;
+    private String modele;
+    private Date annee;
+    private String immatriculation;
+
+    @ManyToMany
+    private Set<PieceAuto> PieceAutos;
+
+    public Vehicule() {}
+
+    public Vehicule(String marque, String modele, Date annee) {
+        this.marque = marque;
+        this.modele = modele;
+        this.annee = annee;
+    }
+
+    public Vehicule(Long id, String marque, String modele, Date annee, String immatriculation) {
+        this.id = id;
+        this.marque = marque;
+        this.modele = modele;
+        this.annee = annee;
+        this.immatriculation = immatriculation;
+    }
 
     public Long getId() {
         return id;
@@ -18,4 +46,35 @@ public class Vehicule {
         this.id = id;
     }
 
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public Date getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(Date annee) {
+        this.annee = annee;
+    }
+
+    public String getImmatriculation() {
+        return immatriculation;
+    }
+
+    public void setImmatriculation(String immatriculation) {
+        this.immatriculation = immatriculation;
+    }
 }
