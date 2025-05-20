cc31ba9e3334cb62f322745b6f2f60a75a24ae40
jeremy <jeremyhoarau44@gmail.com>
2025-05-20 15:33:04 +0400

    Modification d'ApiGestionPieceAutoApplicationTests.java, d'index.html et PieceAutoService

--- src/test/java/com/example/apigestionpieceauto/ApiGestionPieceAutoControllerApplicationTests.java
+++ src/test/java/com/example/apigestionpieceauto/ApiGestionPieceAutoControllerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGestionPieceAutoApplicationTests {
+class ApiGestionPieceAutoControllerApplicationTests {
 
     @Test
     void contextLoads() {
