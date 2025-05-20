f2c9f201c83fbcbc2ba162923291acd28481b93c
jeremy <jeremyhoarau44@gmail.com>
2025-05-20 15:33:17 +0400

    Modification d'ApiGestionPieceAutoApplicationTests.java, d'index.html et PieceAutoService

--- src/main/java/com/example/apigestionpieceauto/service/PieceAutoService.java
+++ src/main/java/com/example/apigestionpieceauto/service/PieceAutoService.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-class PieceAutoService {
+public class PieceAutoService {
 
     @Autowired
     private PieceAutoRepository pieceAutoRepository;
