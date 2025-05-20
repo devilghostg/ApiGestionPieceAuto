bd30eb1c763759a579ca69f70e7c4f4035ad3822
devilghostg <mister.devil6@gmail.com>
2025-05-20 15:15:21 +0400

    add page home / fournisseur

--- src/main/java/com/example/apigestionpieceauto/controller/FournisseurController.java
+++ src/main/java/com/example/apigestionpieceauto/controller/FournisseurController.java
@@ -1,19 +1,26 @@
 package com.example.apigestionpieceauto.controller;
 
+import com.example.apigestionpieceauto.Entity.Fournisseur;
+import com.example.apigestionpieceauto.repository.FournisseurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/fournisseur")
 public class FournisseurController {
-    @GetMapping
-    public String getFournisseur() {
 
+    @Autowired
+    private FournisseurRepository fournisseurRepository;
+
+    @GetMapping
+    public String getFournisseur(Model model) {
+        model.addAttribute("fournisseurs", fournisseurRepository.findAll());
         return "fournisseur/index";
     }
 
-    
     @GetMapping("/all")
     public String getFournisseurAll() {
         return "fournisseur/all";
@@ -28,5 +35,4 @@ public class FournisseurController {
     public String getFournisseurBois() {
         return "fournisseur/bois";
     }
-
 }
