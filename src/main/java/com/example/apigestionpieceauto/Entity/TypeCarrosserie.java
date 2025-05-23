cc8415a0833c7258d97c3fb3048a3d41da2210b1
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2025-05-23 10:19:36 +0400

    Modification de TypeCarrosserie

--- src/main/java/com/example/apigestionpieceauto/Entity/TypeCarrosserie.java
+++ src/main/java/com/example/apigestionpieceauto/Entity/TypeCarrosserie.java
@@ -1,9 +1,19 @@
 package com.example.apigestionpieceauto.Entity;
 
 public enum TypeCarrosserie {
-    ACIER,
-    ALUMINIUM,
-    PLASTIQUE,
-    CABORNE,
-    FIBRE
+    ACIER("Acier"),
+    ALUMINIUM("Aluminium"),
+    PLASTIQUE("Platique"),
+    CABORNE("Caborne"),
+    FIBRE("Fibre");
+
+    public final String label;
+
+    TypeCarrosserie(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
