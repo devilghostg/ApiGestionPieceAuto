cc770d2fca16905ffdff9d6b997ce567e5475b53
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2025-05-23 15:49:32 +0400

    Modification de TypeMoteur

--- src/main/java/com/example/apigestionpieceauto/Entity/TypeMoteur.java
+++ src/main/java/com/example/apigestionpieceauto/Entity/TypeMoteur.java
@@ -1,9 +1,20 @@
 package com.example.apigestionpieceauto.Entity;
 
 public enum TypeMoteur {
-    MANUEL,
-    AUTOMATIQUE,
-    ELECTRIQUE,
-    HYBRID,
-    GAZOLINE
+    MANUEL("Manuel"),
+    AUTOMATIQUE("Automatique"),
+    ELECTRIQUE("Electrique"),
+    HYBRID("Hybrid"),
+    GAZOLINE("Gazoline");
+
+    public final String label;
+
+    TypeMoteur(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
 }
